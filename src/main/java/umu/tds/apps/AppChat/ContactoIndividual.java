94b7e5e4cb082f25499b84391e284b3126d4c077
Emilio <emiliogfp99@gmail.com>
2025-03-05 13:50:45 +0100

    Constructors and getters/setters

--- src/main/java/umu/tds/apps/AppChat/ContactoIndividual.java
+++ src/main/java/umu/tds/apps/AppChat/ContactoIndividual.java
@@ -1,5 +1,19 @@
 package umu.tds.apps.AppChat;
 
-public class ContactoIndividual {
-
+public class ContactoIndividual extends Contacto {
+	
+	private String numero;
+	
+	public ContactoIndividual(String numero, String nombre) {
+		super(nombre);
+		this.numero = numero;
+	}
+	
+	public String getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
 }
--- src/main/java/umu/tds/apps/AppChat/Grupo.java
+++ src/main/java/umu/tds/apps/AppChat/Grupo.java
@@ -1,5 +1,21 @@
 package umu.tds.apps.AppChat;
 
-public class Grupo {
+import java.util.ArrayList;
 
+public class Grupo extends Contacto{
+
+	private ArrayList<ContactoIndividual> contactos;
+	
+	public Grupo(String nombre) {
+		super(nombre);
+		this.contactos = new ArrayList<>();
+	}
+	
+	public void agregarContactos(ContactoIndividual contacto) {
+		contactos.add(contacto);
+	}
+	
+	public ArrayList<ContactoIndividual> getMiembros(){
+		return contactos;
+	}
 }
